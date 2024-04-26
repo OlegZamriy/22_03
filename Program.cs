6e6430d31a8b10f3f9aba2bbbbcb9380500dde86
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:50:15 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int[], IEnumerable<int>> findOddNumbers = numbers => numbers.Where(num => num % 2 != 0);
+
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var oddNumbers = findOddNumbers(array);
+
+        Console.WriteLine("Непарні числа в масиві:");
+        foreach (var num in oddNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
