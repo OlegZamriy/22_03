05ab190fbb62ab4c02196063e81dff166c51a7b5
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:41:46 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 7, 14, 21, 30, 35, 42, 49, 50, 63, 70 };
+
+        Func<int[], int> countMultiplesOfSeven = nums => nums.Count(num => num % 7 == 0);
+
+        int count = countMultiplesOfSeven(numbers);
+
+        Console.WriteLine($"Кількість чисел у масиві, кратних семи: {count}");
+    }
+}
