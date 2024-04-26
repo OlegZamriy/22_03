50783e6763d6eb3504b2a71e3aff23fd2b1042f5
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:42:20 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { -5, 10, -15, 20, 25, -30, 35, -40, 45 };
+
+        Func<int[], int> countPositiveNumbers = nums => nums.Count(num => num > 0);
+
+        int count = countPositiveNumbers(numbers);
+
+        Console.WriteLine($"Кількість позитивних чисел у масиві: {count}");
+    }
+}
