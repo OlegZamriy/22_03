9bcb1bdf155650ec566c00291f421d24acca7a99
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:47:05 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int, int> square = delegate (int num)
+        {
+            return num * num;
+        };
+
+        int numberToSquare = 5;
+
+        int squaredNumber = square(numberToSquare);
+
+        Console.WriteLine($"Квадрат числа {numberToSquare} дорівнює {squaredNumber}.");
+    }
+}
