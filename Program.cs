6d14cf05bac8b894ea5fbb553ee891d691650c73
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:42:52 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { -5, -10, -15, 20, -25, -30, 35, -40, 45, -5, -10 };
+
+        Func<int[], IEnumerable<int>> uniqueNegativeNumbers = nums => nums.Where(num => num < 0).Distinct();
+
+        var uniqueNegatives = uniqueNegativeNumbers(numbers);
+
+        Console.WriteLine("Унікальні негативні числа у масиві:");
+        foreach (var num in uniqueNegatives)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
