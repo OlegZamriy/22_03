9d6686a11da6b65043147d815d65ddbea0124049
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:49:35 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int[], int> findMin = numbers => numbers.Min();
+
+        int[] array = { 5, 10, 15, 20, 25 };
+
+        int minNumber = findMin(array);
+
+        Console.WriteLine($"Мінімальне число в масиві: {minNumber}");
+    }
+}
