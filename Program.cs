4e7ada9c3d8a83a97a55e5b0383fb5c096fd4004
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:48:54 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int[], int> findMax = numbers => numbers.Max();
+
+        int[] array = { 5, 10, 15, 20, 25 };
+
+        int maxNumber = findMax(array);
+
+        Console.WriteLine($"Максимальне число в масиві: {maxNumber}");
+    }
+}
