8f97f55e849aa7071c78900a7724e9120f323807
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:43:37 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<string, string, bool> containsWord = (text, word) =>
+        {
+            string[] words = text.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string w in words)
+            {
+                if (w.Equals(word, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        string inputText = "Це приклад тексту для перевірки наявності слова у ньому.";
+        string searchWord = "приклад";
+
+        bool contains = containsWord(inputText, searchWord);
+
+        if (contains)
+        {
+            Console.WriteLine($"Текст містить слово '{searchWord}'.");
+        }
+        else
+        {
+            Console.WriteLine($"Текст не містить слово '{searchWord}'.");
+        }
+    }
+}
