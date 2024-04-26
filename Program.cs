4c81c8ca10ff20903c5aada841bcca641e9c7255
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:47:40 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int, int> cube = num => num * num * num;
+
+        int numberToCube = 3;
+
+        int cubedNumber = cube(numberToCube);
+
+        Console.WriteLine($"Куб числа {numberToCube} дорівнює {cubedNumber}.");
+    }
+}
