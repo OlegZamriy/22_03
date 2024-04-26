1955e74fa4be22c303fc8409fcb36bf3789ce2f2
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:48:18 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<DateTime, bool> isProgrammersDay = date => date.DayOfYear == 256;
+
+        // Тестування лямбда-виразу
+        DateTime testDate1 = new DateTime(2023, 9, 13);
+        DateTime testDate2 = new DateTime(2024, 9, 12);
+
+        Console.WriteLine($"Дата {testDate1.ToShortDateString()} - день програміста? {isProgrammersDay(testDate1)}");
+        Console.WriteLine($"Дата {testDate2.ToShortDateString()} - день програміста? {isProgrammersDay(testDate2)}");
+    }
+}
