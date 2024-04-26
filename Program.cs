5b91cbc81c09d9c75b7d79a67e172323c5aef539
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 18:46:17 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Func<int, bool> isEven = delegate (int number)
+        {
+            return number % 2 == 0;
+        };
+
+        int numToCheck = 10;
+
+        bool isNumEven = isEven(numToCheck);
+
+        if (isNumEven)
+        {
+            Console.WriteLine($"{numToCheck} - парне число.");
+        }
+        else
+        {
+            Console.WriteLine($"{numToCheck} - непарне число.");
+        }
+    }
+}
